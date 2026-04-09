f05bf6c0396cb414ad96454ea258eeac6325236d
prodsaru <junran.wang393@gmail.com>
2026-04-09 20:20:44 +0800

    Renamed homeowner chat page to simple chat page, modified tool bar config

--- src/views/ChatPage.vue
+++ src/views/ChatPage.vue
@@ -1,17 +1,20 @@
 <template>
-    <ToolBarHomeowner/>
+    <ToolBarHomeowner v-if="userType === 'homeowner'" />
+    <ToolBarContractor v-else-if="userType === 'contractor'" />
     <ChatFunction/>
 </template>
 
 <script>
     import ChatFunction from "@/components/ChatFunction.vue"
     import ToolBarHomeowner from "@/components/ToolBarHomeowner.vue"
+    import ToolBarContractor from "@/components/ToolBarContractor.vue"
     import { useRouter } from "vue-router"
     const router = useRouter()
     export default {
         name:'chat',
         components:{
             ToolBarHomeowner,
+            ToolBarContractor,
             ChatFunction
         }
     }
